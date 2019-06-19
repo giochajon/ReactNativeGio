568c60ce7d6da34635dc96c38c31cd67c7c98257
Giovanni Chajon <giochajon@gmail.com>
2019-06-18 20:06:31 -0600

    imagetest with elements  added

--- imagetest/elements-test/App.js
+++ imagetest/elements-test/App.js
@@ -1,30 +1,55 @@
 import React, { Component } from 'react';
-import { AppRegistry, View, Image, StyleSheet  } from 'react-native';
+//import { AppRegistry, View, Image, StyleSheet  } from 'react-native';
+import { AppRegistry, View,  StyleSheet, ActivityIndicator  } from 'react-native';
+
+import { Image, Button } from 'react-native-elements';
+  
 
 export default function App() {
 
-  const styles = StyleSheet.create({
-    stretch: {  width: 340,     height: 500     },
-    //flex:1, height: undefined, width: undefined
-    //alignSelf: 'stretch',
-    //backgroundColor:"blue",
-  });
+  // const styles = StyleSheet.create({
+  //   stretch: {  width: 340,     height: 500     },
+  //   //flex:1, height: undefined, width: undefined
+  //   //alignSelf: 'stretch',
+  //   //backgroundColor:"blue",
+  // });
 
 
-      return (
-        <View style={{backgroundColor:"blue"}}>
+  //     return (
+  //       <View style={{backgroundColor:"blue"}}>
 
-          <Image
-            style={styles.stretch}
-            //style={{width: 300, height: 600}}
+  //         <Image
+  //           style={styles.stretch}
+  //           //style={{width: 300, height: 600}}
             
-            //style={{flex:1, height: undefined, width: undefined}}
-            source={{uri: 'https://s3-us-west-2.amazonaws.com/public.prod.rainforestab.ca/images/imageManager/rainforest-social-contract2.png'}}
-            resizeMode="contain"
-          />
+  //           //style={{flex:1, height: undefined, width: undefined}}
+  //           source={{uri: 'https://s3-us-west-2.amazonaws.com/public.prod.rainforestab.ca/images/imageManager/rainforest-social-contract2.png'}}
+  //           resizeMode="contain"
+  //         />
          
-        </View>
-      );
-    }
+  //       </View>
+  //     );
+  //   }
+
+  
+  // Standard Image
+ 
+ return(
+  <View style={{backgroundColor:"blue"}}>
+  <Image
+    source={{ uri: "https://s3-us-west-2.amazonaws.com/public.prod.rainforestab.ca/images/imageManager/rainforest-social-contract2.png" }}
+    style={{width: 300, height: 600}}
+
+    
+    resizeMode="contain"
+  />
 
 
+  <Button
+  title="I agree"
+  type="outline"
+/>
+</View>
+ 
+  );
+}
